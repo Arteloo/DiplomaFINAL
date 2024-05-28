a1d71f84a3efd2111b4d492fd17e37c3484ea5a7
Arteloo <arhipovartem509@gmail.com>
2024-05-28 15:37:36 +0300

    navbar and auth page done

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,18 +4,24 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Button, Container} from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import { MAIN_ROUTE } from '../utils/consts';
+import { LOGIN_ROUTE, MAIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 import {observer} from 'mobx-react-lite'
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
     return(
-        <Navbar bg="dark" variant='dark'>
+        <Navbar bg="dark" variant='dark' className='sticky-top'>
             <Container>
                 <NavLink  style={{color:'white', textDecoration: 'none'}} to={MAIN_ROUTE}>RefraMap</NavLink>
                     {user.isAdmin ? 
                         <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={'outline-light'} className="ms-3">Админ панель</Button>
+                        <div className='dropdown'>
+                            <Button className='btn dropdown-toggle' bg='dark' variant={'outline-light'} id='dropDownMenu1' data-bs-toggle="dropdown" aria-expanded="false">Админ панель</Button>
+                            <ul className='dropdown-menu'>
+                                <li><a href='#' className='dropdown-item'>Добавить огнеупор</a></li>
+                                <li><a href='#' className='dropdown-item'>Добавить агрегат</a></li>
+                            </ul>
+                        </div>
                         <Button variant={'outline-light'} className="ms-3">Выйти</Button>
                         </Nav>
                     : user.isAuth ?
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Button, Card, Container, Row, Form } from 'react-bootstrap';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 const Auth = () => {
+    const loc = useLocation()
+    const isLogin = loc.pathname === LOGIN_ROUTE
+    console.log(loc)
     return (
-        <div>
-            AUTH
-        </div>
+        <Container className="d-flex justify-content-center align-items-center" style={{height: window.innerHeight - 54}}>
+            <Card className='p-5'>
+                <Form>
+                <h2 className='d-flex m-auto justify-content-center align-items-center mb-3'>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                    <Form.Control className='mt-2' placeholder='Введите ваш e-mail...'/>
+                    <Form.Control className='mt-2' placeholder='Введите ваш пароль'/>
+                    <Row className='d-flex justify-content-evenly'>
+                    {isLogin ? 
+                    <div className='d-flex justify-content-between mt-1 mb-3'>Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрироваться</NavLink></div>
+                    : 
+                    <div className='d-flex justify-content-between mt-1 mb-3'>Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите</NavLink></div>
+                    }
+                    <Button variant='outline-success'>
+                        {isLogin ? 'Войти' : 'Регистрация'}
+                    </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
